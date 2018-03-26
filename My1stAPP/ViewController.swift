a1a8c497f415b0641c64ecef318ae63c32bc45d6
Jiang Siao-Huei <shjiang10@Jiangteki-MacBook-Air.local>
2018-03-26 22:15:54 +0800

    Build up 1st APP assignment

---
+++ My1stAPP/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  My1stAPP
+//
+//  Created by Jiang Siao-Huei on 2018/3/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
